32b8fad8ef40dc7c51859b983dbcbea5ccf7c17e
Dimitris Tsipras <tsipras@mit.edu>
2018-06-14 22:33:53 -0700

    Robust-ML defense interface

---
+++ model_robustml.py
@@ -0,0 +1,25 @@
+from keras.applications.resnet50 import ResNet50, preprocess_input, decode_predictions
+import numpy as np
+import robustml
+
+from methods import pixel_deflection, denoiser
+
+class Model(robustml.model.Model):
+  def __init__(self):
+      self._model = ResNet50(weights='imagenet')
+      self._dataset = robustml.dataset.ImageNet(shape=(224,224,3))
+      self._threat_model = robustml.threat_model.L2(epsilon=0.05)
+
+  @property
+  def dataset(self):
+      return self._dataset
+
+  @property
+  def threat_model(self):
+      return self._threat_model
+
+  def classify(self, x):
+      img = pixel_deflection(x, np.zeros(x.shape[:2]), 200, 10, 0.04)
+      img = denoiser('wavelet', img/255.0, 0.04)*255.0
+      img = preprocess_input(np.stack([img],axis=0))
+      return np.argmax(self._model.predict(img)[0])
